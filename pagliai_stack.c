5e87e622aa328d8afb2f955cbfbe19d1fe50c567
AnthonyP06 <a.pagliai06@gmail.com>
2015-03-25 08:52:12 +0100

    pop and push Added + Update of create and destroy
    Push : push a new element at the top of the stack
    Pop : pop the top element of the stack
    
    Modification : Bug of the .exe programm because I forgot to allocate the
    unsigned int* pointer associated with the table of values of the stack.
    In the function 'destroy', I have just added 1 line to free to memory
    allocated for the table of values

--- pagliai_stack.c
+++ pagliai_stack.c
@@ -9,7 +9,7 @@ struct uint_stack
 {
   unsigned int size;
 	unsigned int* values;
-	unsigned int position;
+	unsigned int nb_elements;
 };
 
 typedef struct uint_stack uint_stack_t;
@@ -18,39 +18,76 @@ typedef struct uint_stack uint_stack_t;
 // Return NULL if the creation didn't succeed (for a lack of memory)
 uint_stack_t* uint_stack_create(size_t nb)
 {
-  // Allocate the new stack
-	uint_stack_t* stack = malloc(sizeof(unsigned int)*nb);
+  // Allocate the new table of values
+	unsigned int* values = malloc(sizeof(unsigned int)*nb);
 	
 	// Did the allocation succeed ?
-	if (stack != NULL)
-	{
-		stack -> size = nb;
-		stack -> values = NULL;
-		stack -> position = 0;
-		
-		// Everything is OK, return the new stack
-		return stack;
-	}
-	
-	else
+	if (values == NULL)
 	{
 		// Error of allocation (lack of memory)
 		return NULL;
 	}
+		
+	// Allocate the new stack
+	uint_stack_t* stack = malloc(sizeof(uint_stack_t));
+		
+	// Did the stack allocation succeed ?
+	if(stack == NULL)
+	{
+		free(values);
+		return NULL;
+	}
+		
+	// Everything is OK	
+	// Init the table of values
+	for (unsigned int i = 0; i < nb; ++i)
+	{
+		values[i] = 0;
+	}
+	
+	stack -> size = nb;
+	stack -> values = values;
+	stack -> nb_elements = 0;
+	
+	return stack;
 }
 
 // Free the memory used by a stack.
 // After this call, the free'd stack can no longer be used.
 void uint_stack_destroy(uint_stack_t* stack)
 {
-  free(stack);
+  free(stack->values);
+	free(stack);
 }
 
 // Copy src into dst.
 // Return -1 if src is too big to be copied into dst, 0 otherwise.
 /* int uint_stack_copy(uint_stack_t* src, uint_stack_t* dst)
 {
-  // TO DO
+  // Is the stack src too big to be copied into dst ?
+	if(src->size > dst->size)
+	{
+		return -1;
+	}
+	
+	else
+	{
+		// All values in dst are deleted
+		dst->values = NULL;
+		dst->nb_elements = 0;
+		
+		// Values of src copied in dst
+		for(unsigned int i=0; i<src->size; ++i)
+		{
+			dst->values[i] = src->values[i];
+		}
+		
+		// Update of the new nb_elements for the stack dst
+		dst->nb_elements = src->nb_elements;
+		
+		// Everything is OK
+		return 0;
+	}
 } */
 
 // Swap the content of two stacks.
@@ -62,25 +99,51 @@ void uint_stack_destroy(uint_stack_t* stack)
 // Push a value.
 // If the stack is full, it's not modified.
 // Return 0 if the stack wasn't full, -1 otherwise.
-/* int uint_stack_push(uint_stack_t* stack, unsigned int element)
+int uint_stack_push(uint_stack_t* stack, unsigned int element)
 {
-  // TO DO
-} */
+  // Is the stack full ?
+	if(stack->nb_elements == stack->size)
+	{
+		return -1;
+	}
+	
+	else
+	{
+		stack->values[stack->nb_elements] = element;
+		stack->nb_elements++; 
+		
+		// Everything is OK
+		return 0;
+	}
+}
 
 // Pop a value.
 // If the stack is empty, it's not modified.
-// Return 0 if the stack wasn't empty, -1 otherwise.
-/* int uint_stack_pop(uint_stack_t* stack, unsigned int* element)
+// Return the top element if the stack wasn't empty, -1 otherwise.
+int uint_stack_pop(uint_stack_t* stack)
 {
-  // TO DO
-} */
+  // Is the stack empty ?
+	if(stack->nb_elements == 0)
+	{
+		return -1;
+	}
+	
+	else
+	{
+		stack->nb_elements--;
+		unsigned int value = stack->values[stack->nb_elements];
+		
+		// Everything is OK
+		return value; 
+	}
+}
 
 // Return the size of a stack.
 // This is the actual number of stored elements, not the maximal size.
-/* size_t uint_stack_size(uint_stack_t* stack)
+size_t uint_stack_size(uint_stack_t* stack)
 {
-  // TO DO
-} */
+  return stack->nb_elements;
+}
 
 /* ------------------------------------------------------- */
 
@@ -112,7 +175,7 @@ int main(int argc, char** argv)
     uint_stack_destroy(stack2);
   
 	} 
-  /* // Check a simple push()/pop().
+  // Check a simple push()/pop().
   {
     uint_stack_t* stack = uint_stack_create(5);
     CHECK(uint_stack_size(stack) == 0);
@@ -121,7 +184,7 @@ int main(int argc, char** argv)
     CHECK(uint_stack_size(stack) == 1);
 
     unsigned int value;
-    uint_stack_pop(stack, &value);
+    value = uint_stack_pop(stack);
     CHECK(uint_stack_size(stack) == 0);
     CHECK(value == 42);
 
@@ -129,7 +192,7 @@ int main(int argc, char** argv)
   }
 
   // Test with a stack of size 1.
-  {
+  /* {
     uint_stack_t* stack = uint_stack_create(1);
 
     uint_stack_push(stack, 101);
@@ -140,10 +203,10 @@ int main(int argc, char** argv)
     CHECK(value == 101);
     CHECK(uint_stack_size(stack) == 0);
     uint_stack_destroy(stack);    
-  }
+  } */
 
   // Check multiple push()/pop().
-  {
+  /* {
     uint_stack_t* stack = uint_stack_create(10);
 
     size_t i = 0;
@@ -162,10 +225,10 @@ int main(int argc, char** argv)
     CHECK(uint_stack_size(stack) == 0);
 
     uint_stack_destroy(stack);
-  }
+  } */
 
   // Check that push() doesn't modify a full stack.
-  {
+  /* {
     uint_stack_t* stack = uint_stack_create(10);
 
     size_t i = 0;
@@ -182,10 +245,10 @@ int main(int argc, char** argv)
     }
 
     uint_stack_destroy(stack);
-  }
+  } */
 
   // Test swap().
-  {
+  /* {
     unsigned int value;
 
     uint_stack_t* stack1 = uint_stack_create(5);
@@ -212,10 +275,10 @@ int main(int argc, char** argv)
 
     uint_stack_destroy(stack1);
     uint_stack_destroy(stack2);
-  }
+  } */
 
   // Test copy().
-  {
+  /* {
     unsigned int value;
 
     uint_stack_t* stack1 = uint_stack_create(5);
@@ -240,7 +303,7 @@ int main(int argc, char** argv)
 
     uint_stack_destroy(stack1);
     uint_stack_destroy(stack2);
-  }*/
+  } */
 
 	// Number of failures.
   printf("%lu failure(s)\n", nb_failures);
